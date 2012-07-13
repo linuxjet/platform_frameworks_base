c7bde18af1314ff53857dbee50e30b6029142c14
Ryan Welton <rwelton24@gmail.com>
2012-07-13 14:00:00 +0100

    Merged changes from Rayn Welton on CM Extended Power Menu - Reboot
    Ported the reboot menu from CM9 to CM10, the original patch was not
    clean and require a bit of re-work.
    
    Change-Id: I70cf515f097ea94aedaa7469b0cb18c797af4b57
    
    Extended Power Menu: Initialize reboot reason to the first reason
    Change-Id: Id94863d56c45ff10d3ded7f62e3f2ef7b8cd0e69
    
    Extended Power Menu - Reboot
    Change-Id: I8c3a773ba943dc42e8cf3feab32c462d25d3bb06

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -5154,6 +5154,12 @@ public class WindowManagerService extends IWindowManager.Stub
 
     // Called by window manager policy.  Not exposed externally.
     @Override
+    public void reboot() {
+        ShutdownThread.reboot(mContext, null, true);
+    }
+
+    // Called by window manager policy.  Not exposed externally.
+    @Override
     public void rebootSafeMode() {
         ShutdownThread.rebootSafeMode(mContext, true);
     }
